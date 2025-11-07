46d07e555df162927db49779228dd96fcb0c5df1
AlphaV2 <dominichemal@gmail.com>
2025-11-07 18:05:25 +0530

    Initial commit: Django REST Framework JWT login API implemented

---
+++ api/views.py
@@ -0,0 +1,15 @@
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth import authenticate
+
+class LoginView(APIView):
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        user = authenticate(username=username, password=password)
+        if user:
+            refresh = RefreshToken.for_user(user)
+            return Response({"access": str(refresh.access_token)}, status=200)
+        return Response({"error": "Invalid Credentials"}, status=401)
